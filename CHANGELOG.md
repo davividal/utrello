783c6a8100d59f3bad0f0dc15d437fa02d36a014
semantic-release <semantic-release>
2022-06-25 23:46:18 +0200

    0.0.2
    Automatically generated by python-semantic-release

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Changelog
+
+<!--next-version-placeholder-->
+
+## v0.0.2 (2022-06-25)
+
--- src/utrello/__init__.py
+++ src/utrello/__init__.py
@@ -5,7 +5,7 @@ import os
 from . import api, model
 
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 
 def board_list_handler(api_credentials, args):
